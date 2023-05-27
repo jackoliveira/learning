d59463d405357c04f5961ecc31cde7ad272cf9f7
jackoliveira <jakku@outlook.com.br>
2023-05-27 18:08:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# learning
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-nhsavy)
---
+++ script.js
@@ -0,0 +1,54 @@
+const busca = document.getElementById('busca');
+const listaResultadosUL = document.getElementById('lista-resultados');
+
+let buscaValor = '';
+let listaDeResultados = [];
+const valoresDaBusca = [
+  {
+    name: 'javascript',
+    link: 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript',
+  },
+  {
+    name: 'java',
+    link: 'https://www.java.com/pt-BR/',
+  },
+  {
+    name: 'ruby',
+    link: 'https://www.ruby-lang.org/pt/',
+  },
+  {
+    name: 'rust',
+    link: 'https://www.rust-lang.org/pt-BR',
+  },
+  {
+    name: 'haskell',
+    link: 'https://www.haskell.org/',
+  },
+];
+
+function buscaValores(stringBusca) {
+  return valoresDaBusca.filter((value) => {
+    return value.name.includes(buscaValor);
+  });
+}
+
+busca.addEventListener('input', (event) => {
+  buscaValor = event.target.value;
+
+  listaDeResultados = buscaValores(buscaValor);
+
+  listaResultadosUL.innerHTML = '';
+  if (listaDeResultados.length > 0) {
+    listaDeResultados.forEach((itemDaLista) => {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.innerHTML = itemDaLista.name;
+      a.href = itemDaLista.link;
+      a.target = '_blank';
+      li.appendChild(a);
+      listaResultadosUL.appendChild(li);
+    });
+  }
+
+  console.log(listaDeResultados);
+});
